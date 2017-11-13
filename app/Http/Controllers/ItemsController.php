87f7ab57feadb5cea66a56c17bfbfbbb683439bb
mt551 <mmmq551@gmail.com>
2017-11-13 05:40:34 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -28,3 +28,14 @@ public function create()
             'items' => $items,
         ]);
     }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
